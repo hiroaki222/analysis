9cd664ee9431b73608f6d79a841a0d3bad8081a6
hiroaki222 <hiroaki.takahara.222@gmail.com>
2023-05-20 02:46:32 +0900

    欠損値埋めた

--- classification/main.py
+++ classification/main.py
@@ -1,9 +1,22 @@
+from sklearn import tree
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-train = pd.read_csv('train.csv')
-test = pd.read_csv('test.csv')
+pd.set_option('display.max_rows', None)
 
-#print(train.isnull().sum())
-#print(train)
+df = pd.read_csv('train.csv')
+ans = pd.read_csv('test.csv')
+
+# 要らんカラムを落とす
+df = df[["Survived","Pclass","Sex","Age","SibSp","Parch","Fare","Cabin","Embarked"]]
+
+# stringをintに
+df.replace({'Sex': {'male': 0, 'female': 1}}, inplace=True)
+df.replace({'Embarked': {'S': 0, 'C': 1, 'Q':2}}, inplace=True)
+# これは適当に中央値で埋める
+df['Embarked'].fillna(df['Embarked'].median(), inplace=True)
+# PclassごとのAgeの平均値で埋める
+df['Age'] = df.groupby('Pclass').transform(lambda x: x.fillna(x.mean()))['Age']
